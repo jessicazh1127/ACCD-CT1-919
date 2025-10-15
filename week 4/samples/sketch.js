7f031c1f1c966d4673c0871f38691e572f209d61
jessicazh1127 <jessicazh1127@gmail.com>
2025-10-15 15:41:21 -0700

    10.15

---
+++ week 4/samples/sketch.js
@@ -0,0 +1,48 @@
+let posX
+let posY
+
+let velX
+let velY
+
+let radius = 20
+
+let imgBlack
+let imgWhite
+
+function preload(){
+  imgBlack = loadImage("black.png")
+  imgWhite = loadImage("white.png")
+
+}
+
+function setup() {
+  createCanvas(500, 500);
+  
+  posX = random (radius, height - radius)
+  posY = random (radius, height - radius)
+
+  velX = random (-6, 6)
+  velY = random (-6, 6)
+  
+}
+
+function draw() {
+  background(230);
+
+  posX = velX + posX 
+  posY = velY + posY
+
+  if (posX + radius>=width || posX - radius<=0){
+    velX = -velX 
+  }
+
+  if (posY + radius>=height || posY - radius<=0){
+    velY = -velY
+  }
+
+
+  noStroke()
+
+  circle(posX, posY, radius*2)
+  image(imgWhite, posX, posY)
+}
